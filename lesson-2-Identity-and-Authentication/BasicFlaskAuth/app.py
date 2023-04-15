b1082a49b134d0b617dc3e32853bd806bb6b895b
Tung Nguyen <hoangtungtcc@gmail.com>
2023-04-15 15:42:29 +0700

    Add authorization

--- lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
+++ lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
@@ -104,21 +104,40 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+                        raise AuthError({
+                            'code': 'invalid_claims',
+                            'description': 'Permissions not included in JWT.'
+                        }, 400)
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+    
+    return True
+    
+def requires_auth(permission = ''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
 
-    return wrapper
+            check_permissions(permission, payload)    
+            
+            return f(payload, *args, **kwargs)
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
-    print(payload)
+        return wrapper
+    return requires_auth_decorator
+
+@app.route('/image')
+@requires_auth('get:image')
+def images(jwt):
+    print(jwt)
     return 'Access Granted'
